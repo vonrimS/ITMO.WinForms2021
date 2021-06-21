de101e49993f861460ca28144ebb12d2fc16771f
vonrimS <mr.mytab@gmail.com>
2021-06-21 16:17:30 +0300

    Lab02 Exercise08

---
+++ Lab02 Exercise08/RegistrationForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab02_Exercise08
+{
+    public partial class RegistrationForm : Form
+    {
+        public RegistrationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                Label lbl = new Label();
+                lbl.Location = new System.Drawing.Point(26, 126);
+                lbl.Size = new System.Drawing.Size(32, 23);
+                lbl.Name = "labelll";
+                lbl.TabIndex = 2;
+                lbl.Text = "PIN2";
+                groupBox1.Controls.Add(lbl);
+                TextBox txt = new TextBox();
+                txt.Location = new System.Drawing.Point(92, 119);
+                txt.Size = new System.Drawing.Size(220, 20);
+                txt.Name = "textboxx";
+                txt.TabIndex = 1;
+                txt.Text = "";
+                groupBox1.Controls.Add(txt);
+            }
+            else
+            {
+                int lcv;
+                lcv = groupBox1.Controls.Count;// определяется количество элементов
+                while (lcv > 4)
+                {
+                    groupBox1.Controls.RemoveAt(lcv - 1);
+                    lcv -= 1;
+                }
+            }
+        }
+
+        private void RegistrationForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Поле Name не может содержать цифры");
+                errorProvider1.SetError(textBox1, "Must be letter");
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Поле PIN не может содержать буквы");
+            }
+        }
+    }
+}
