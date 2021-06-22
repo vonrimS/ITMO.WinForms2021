7253a1872b136d0f7301612e23f804eb0f2f12c4
vonrimS <mr.mytab@gmail.com>
2021-06-22 12:20:42 +0300

    Lab03 Exercise03

---
+++ Lab03 Exercise03/WinButNum.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Lab03_Exercise03
+{
+    partial class WinButNum
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clickButton1 = new Lab03_Exercise03.ClickButton();
+            this.SuspendLayout();
+            // 
+            // clickButton1
+            // 
+            this.clickButton1.Location = new System.Drawing.Point(58, 54);
+            this.clickButton1.Name = "clickButton1";
+            this.clickButton1.Size = new System.Drawing.Size(134, 23);
+            this.clickButton1.TabIndex = 0;
+            this.clickButton1.Text = "clickButton1";
+            this.clickButton1.UseVisualStyleBackColor = true;
+            // 
+            // WinButNum
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clickButton1);
+            this.Name = "WinButNum";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ClickButton clickButton1;
+    }
+}
+
