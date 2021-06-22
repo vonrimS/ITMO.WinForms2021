1ca8a023fb3cfb7df5714ec635b274e03e92b2f5
vonrimS <mr.mytab@gmail.com>
2021-06-22 15:09:19 +0300

    Lab04 Exercise02

---
+++ Lab04 Exercise02/EditPerson.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace Lab04_Exercise02
+{
+    partial class EditPerson
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.personsListView = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // personsListView
+            // 
+            this.personsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.personsListView.HideSelection = false;
+            this.personsListView.Location = new System.Drawing.Point(13, 13);
+            this.personsListView.Name = "personsListView";
+            this.personsListView.Size = new System.Drawing.Size(560, 255);
+            this.personsListView.TabIndex = 0;
+            this.personsListView.UseCompatibleStateImageBehavior = false;
+            this.personsListView.View = System.Windows.Forms.View.Details;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(312, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(260, 60);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Редактировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Имя";
+            this.columnHeader1.Width = 186;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Фамилия";
+            this.columnHeader2.Width = 186;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Возраст";
+            this.columnHeader3.Width = 180;
+            // 
+            // EditPerson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.personsListView);
+            this.Name = "EditPerson";
+            this.Text = "Список сотрудников";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView personsListView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ Lab04 Exercise02/EditPerson.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab04_Exercise02
+{
+    public partial class EditPerson : Form
+    {
+        public EditPerson()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EditPersonForm editForm = new EditPersonForm();
+            if (editForm.ShowDialog() != DialogResult.OK)
+                return;
+            ListViewItem newItem = personsListView.Items.Add(editForm.FirstName);
+            newItem.SubItems.Add(editForm.LastName);
+            newItem.SubItems.Add(editForm.Age.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (personsListView.SelectedItems.Count == 0)
+                return;
+            ListViewItem item = personsListView.SelectedItems[0];
+
+            EditPersonForm editForm = new EditPersonForm();
+            editForm.FirstName = item.Text;
+            editForm.LastName = item.SubItems[1].Text;
+            editForm.Age = Convert.ToInt32(item.SubItems[2].Text);
+
+            if (editForm.ShowDialog() != DialogResult.OK)
+                return;
+            item.Text = editForm.FirstName;
+            item.SubItems[1].Text = editForm.LastName;
+            item.SubItems[2].Text = editForm.Age.ToString();
+        }
+    }
+}
