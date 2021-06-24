69e8a8a47bcfe995e7ecbb12f71a3c5bf58b42b4
vonrimS <mr.mytab@gmail.com>
2021-06-24 12:30:35 +0300

    Lab07 Exercise05

---
+++ Lab07 Exercise05/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Lab07_Exercise05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.MaxValue = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(323, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите максимальное число для поиска натуральных чисел:";
+            // 
+            // MaxValue
+            // 
+            this.MaxValue.Location = new System.Drawing.Point(353, 32);
+            this.MaxValue.Name = "MaxValue";
+            this.MaxValue.Size = new System.Drawing.Size(100, 20);
+            this.MaxValue.TabIndex = 1;
+            this.MaxValue.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MaxValue_KeyPress);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(27, 63);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(199, 23);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.Text = "Рассчитать";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Прогресс";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(87, 96);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(366, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 138);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(441, 147);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(254, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Отчистить форму";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 297);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.MaxValue);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Решето Эратосфена";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox MaxValue;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lab07 Exercise05/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ 
+ Разработайте приложение в котором реализуйте вывод в подходящий 
+для этого компонент последовательность чисел по алгоритму Эратосфена, 
+рассчитанных во вторичном потоке. Исходной информацией является передаваемое, 
+например из текстового поля максимальное число последовательности.
+В качестве реализации указанного алгоритма можно использовать метод, 
+в котором последовательность до максимального числа (получаемого из текстового 
+поля MaxValue.Text) формируется и возвращается в виде строки: 
+
+    public string GoButt()
+    { 
+        int maxValue = 0; 
+        System.Text.StringBuilder resultText = new System.Text.StringBuilder();
+        if (int.TryParse(MaxValue.Text, out maxValue)) 
+        {
+            for (int trial = 2; trial <= maxValue; trial++) 
+            { 
+                bool isPrime = true; 
+                for (int divisor = 2; divisor <= Math.Sqrt(trial); divisor++) 
+                { 
+                    if (trial % divisor == 0) 
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime) 
+                { 
+                    resultText.AppendFormat("{0} ", trial); 
+                }
+            } 
+         } else { resultText.Append("Unable to parse maximum value."); 
+    } 
+    return resultText.ToString(); 
+}
+
+ */
+
+namespace Lab07_Exercise05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Visible = false;
+        }
+
+        private delegate int EratosthenesSieve(int a);
+
+        public string GoButt()
+        {
+            int maxValue = 0;
+            System.Text.StringBuilder resultText = new System.Text.StringBuilder();
+            if (int.TryParse(MaxValue.Text, out maxValue))
+            {
+                for (int trial = 2; trial <= maxValue; trial++)
+                {
+                    bool isPrime = true;
+                    for (int divisor = 2; divisor <= Math.Sqrt(trial); divisor++)
+                    {
+                        if (trial % divisor == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        resultText.AppendFormat("{0} ", trial);
+                    }
+                    backgroundWorker1.ReportProgress((int)(trial*100/maxValue));
+
+                }
+            }
+            else
+            {
+                resultText.Append("Unable to parse maximum value.");
+            }
+            return resultText.ToString();
+        }
+
+
+
+        private void MaxValue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Поле должно содержать цифры");
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //System.Threading.Thread.Sleep(1000);
+            //backgroundWorker1.ReportProgress(100);
+            richTextBox1.Text = GoButt();
+
+            //int i;
+            //i = int.Parse(e.Argument.ToString());
+            //for (int j = 1; j <= i; j++)
+            //{
+            //    if (backgroundWorker1.CancellationPending)
+            //    {
+            //        e.Cancel = true;
+            //        return;
+            //    }
+            //    System.Threading.Thread.Sleep(1000);
+            //    backgroundWorker1.ReportProgress((int)(j * 100 / i));
+            //}
+        }
+
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (!(e.Cancelled))
+                System.Windows.Forms.MessageBox.Show("Рассчет завершен!");
+            else
+                System.Windows.Forms.MessageBox.Show("Рассчет прерван");
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (!(MaxValue.Text == ""))
+            {
+                int i = int.Parse(MaxValue.Text);
+                backgroundWorker1.RunWorkerAsync(i);
+                button1.Visible = true;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+    }
+}
